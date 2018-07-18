892ea7d4e048914dc8f432fe6b8979dfd2a64369
calvinrzachman <calvin.zachman@protonmail.com>
2018-07-18 10:23:54 -0500

    GET/POST for transactions

--- app/index.js
+++ app/index.js
@@ -9,15 +9,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain'); // Grabs the index.js file by default
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 // Define a port which our application will listen to requests on
 const HTTP_PORT = process.env.HTTP_PORT || 3001; 
 
 const app = express() ;             // Call default express function - creates an express application with a lot of functionality
 const bchain = new Blockchain();
+const wallet = new Wallet();
+const txPool = new TransactionPool(); 
 const p2pServer = new P2pServer(bchain);
 app.use(bodyParser.json());         // Allows us to receive JSON within POST requests
 
+// Blocks
 // Define GET request
 app.get('/blocks', (req, res) => {
     res.json(bchain.chain);
@@ -31,6 +36,22 @@ app.post('/mine', (req,res) => {
     p2pServer.syncChains();
     res.redirect('/blocks');
 });
+
+// Transactions
+// Define GET request
+app.get('/transactions', (req, res) => {
+    res.json(txPool.transactions); // Create the json form of the transaction in the mem pool and send it back as response 
+});
+
+// Define POST Request
+app.post('/transact', (req, res) => {
+    const { recipient, amount } = req.body;
+    const transaction = wallet.createTransaction(recipient, amount, txPool);
+    // console.log(`Transaction created ${JSON.stringify(transaction)}`);
+    // p2pServer.broadcastTransaction(transaction);
+    res.redirect('/transactions'); // Redirect to existing tx GET endpoint so they can see the new tx
+});
+
 
 app.listen(HTTP_PORT, () => {
     console.log(`Listening on port ${HTTP_PORT}`)
--- config.js
+++ config.js
@@ -4,6 +4,6 @@
 
 const DIFFICULTY = 3;           // Specify the number of leading zeros in the block hash required for valid PoW 
 const MINE_RATE = 3000;         // Approximate block time in milliseconds
-const INITIAL_BALANCE = 100;    // Starting cryptocurrency balance in wallet
+const INITIAL_BALANCE = 500;    // Starting cryptocurrency balance in wallet
 
 module.exports = { DIFFICULTY, MINE_RATE, INITIAL_BALANCE };
