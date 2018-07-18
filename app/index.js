eddd1e692d562067576f513ad4df48320cfff1db
calvinrzachman <calvin.zachman@protonmail.com>
2018-07-18 12:50:11 -0500

    mine-transactions endpoint

--- app/index.js
+++ app/index.js
@@ -11,6 +11,7 @@ const Blockchain = require('../blockchain'); // Grabs the index.js file by defau
 const P2pServer = require('./p2p-server');
 const Wallet = require('../wallet');
 const TransactionPool = require('../wallet/transaction-pool');
+const Miner = require('./miner');
 
 // Define a port which our application will listen to requests on
 const HTTP_PORT = process.env.HTTP_PORT || 3001; 
@@ -20,6 +21,8 @@ const bchain = new Blockchain();
 const wallet = new Wallet();
 const txPool = new TransactionPool(); 
 const p2pServer = new P2pServer(bchain, txPool);
+const miner = new Miner(bchain, txPool, wallet, p2pServer);
+
 app.use(bodyParser.json());         // Allows us to receive JSON within POST requests
 
 // Blocks
@@ -56,6 +59,13 @@ app.post('/transact', (req, res) => {
 app.get('/public-key', (req, res) => {
     res.json( { publicKey: wallet.pubKey });
 });
+
+// Mining 
+app.get('/mine-transactions', (req,res) => {
+    const block = miner.mine();
+    console.log(`New block added: ${JSON.stringify(block, null, 4)}`)
+    res.redirect('/blocks'); // See new mined block
+})
 
 app.listen(HTTP_PORT, () => {
     console.log(`Listening on port ${HTTP_PORT}`)
